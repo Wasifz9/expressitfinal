c628a28c03796477d898c8d07881c0cff038173b
Wasif Zulkernine <48087763+Wasifz9@users.noreply.github.com>
2020-04-29 11:55:15 -0400

    Update express.js

--- static/express.js
+++ static/express.js
@@ -1,25 +1,27 @@
 
 
 var englishWords = ["Circumstance", "Responsible", "Immigration", "Colonel", "Lieutenant", "Threshold", "Sibling",
-                    "To say goodbye", "To dawn", "Threat", "Manslaughter", "Expunged"];
+                    "To say goodbye", "To dawn", "Threat", "Manslaughter", "Expunged", "Embarrassment", "Pregnancy", "Parents", 
+                    "Relatives", "Resilient", "Onus", "Alibi", "Debunk", "Idle", "Children", "Redacted", "Elope", "Parole",
+                   "Escalator", "Fresh produce", "Fingers", "Toes", "Excitement"];
 
 var spanishWords = ["Circumstancia", "Responsable", "Inmigración", "Coronel", "Teniente", "Umbral", "Hermano(a)",
-                    "Despedirse", "Amanecer", "Amenaza", "Homicidio involuntario", "Suprimir"];
+                    "Despedirse", "Amanecer", "Amenaza", "Homicidio involuntario", "Suprimir", "Verguenza", "Embarazo", "Parents", 
+                    "Parientes", "Resiliente", "Responsibility", "Coartada", "Desacreditar ridculizar", "Inmóvil", "Niños(as)",
+                    "Expurgada", "Casarse a escondidas", "Libertad condicional", "Escalera mecánica", "Verduras frescas", 
+                    "Dedos de la manos", "Dedos de los pies", "Exaltación"];
 
 
 var date = new Date();
 var day = date.getDate();
 var loc; 
 
-if (day > 12 && day <= 24){
-    loc = day - 12;
+
+if (day == 31) {
+    day = 15;
 }
 
-else if (day > 24) {
-    day = Math.floor(Math.random() * 12);
-}
-
-loc = loc - 1;
+loc = day - 1;
 
 
 function showenglish() {
